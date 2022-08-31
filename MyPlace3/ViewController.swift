badae417fd35679bf2ee5acba5ca76d7b0f7200b
havvk01 <havvk01@gmail.com>
2022-08-31 16:55:29 +0300

    Initial Commit

---
+++ MyPlace3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlace3
+//
+//  Created by Slava Havvk on 31.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
