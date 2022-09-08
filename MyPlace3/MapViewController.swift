ed6c766e0540594b19698cf854b0678fe2d41181
havvk01 <havvk01@gmail.com>
2022-09-08 15:55:25 +0300

    add mapKit

---
+++ MyPlace3/MapViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MapViewController.swift
+//  MyPlace3
+//
+//  Created by Slava Havvk on 08.09.2022.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func closeVC() {
+
+        dismiss(animated: true)
+
+    }
+    
+
+}
